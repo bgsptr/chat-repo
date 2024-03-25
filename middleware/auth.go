70585daaa43825165536a45e1c5e5f3419ac164c
unknown <agunggusti95@gmail.com>
2024-03-25 17:25:30 +0800

    add config, modify middleware

---
+++ middleware/auth.go
@@ -0,0 +1,17 @@
+package middleware
+
+func ValidateJWT(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return err
+	}
+
+	return nil
+}
